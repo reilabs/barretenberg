0b646b44e73b28884dbcaf83095b103b2ec6e67d
TheMenko <themenkoprojects@gmail.com>
2023-06-20 11:28:46 +0200

    bench functions

---
+++ cpp/src/barretenberg/stdlib/commitment/bench_bindings.cpp
@@ -0,0 +1,46 @@
+#include "bench_bindings.hpp"
+
+#include <barretenberg/plonk/proof_system/types/proof.hpp>
+#include <barretenberg/plonk/proof_system/proving_key/serialize.hpp>
+#include <barretenberg/srs/global_crs.hpp>
+
+using namespace barretenberg::srs;
+
+void build_circuit(UltraPlonkComposer& composer, int circuit_size)
+{
+    while (composer.get_num_gates() <= circuit_size / 2) {
+        plonk::stdlib::pedersen_commitment<UltraPlonkComposer>::compress(field_ct(witness_ct(&composer, 1)),
+                                                                         field_ct(witness_ct(&composer, 1)));
+    }
+}
+
+UltraPlonkComposer* create_composer(int circuit_size)
+{
+    auto composer = std::make_unique<UltraPlonkComposer>(create_prover_factory());
+    build_circuit(*composer, circuit_size);
+
+    if (composer->failed()) {
+        std::string error = format("composer logic failed: ", composer->err());
+        throw_or_abort(error);
+    }
+
+    composer->compute_proving_key();
+
+    return composer.release();
+}
+
+proof commit(UltraPlonkComposer* composer)
+{
+    plonk::stdlib::field_t<plonk::UltraPlonkComposer> left(
+        plonk::stdlib::witness_t(composer, barretenberg::fr::random_element()));
+    plonk::stdlib::field_t<plonk::UltraPlonkComposer> out(
+        plonk::stdlib::witness_t(composer, barretenberg::fr::random_element()));
+
+    // allegedly compress is same as commit, or so i'm ment to believe
+    out = proof_system::plonk::stdlib::pedersen_commitment<plonk::UltraPlonkComposer>::compress(left, out);
+}
+
+std::shared_ptr<barretenberg::srs::factories::CrsFactory> create_prover_factory()
+{
+    return get_crs_factory();
+}
---
+++ cpp/src/barretenberg/stdlib/commitment/bench_bindings.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "barretenberg/plonk/proof_system/types/proof.hpp"
+#include "barretenberg/stdlib/types/ultra.hpp"
+
+using namespace proof_system::plonk;
+using namespace stdlib::types;
+
+std::shared_ptr<barretenberg::srs::factories::CrsFactory> create_prover_factory();
+
+Composer* create_composer(int circuit_size);
