901dc6143d95e31972e61ea27912a6c1099e26ba
Menko <themenkoprojects@gmail.com>
2023-07-12 18:01:20 +0200

    wip

--- cpp/src/barretenberg/stdlib/commitment/bench_bindings.cpp
+++ cpp/src/barretenberg/stdlib/commitment/bench_bindings.cpp
@@ -51,7 +51,8 @@ barretenberg::srs::factories::ProverCrs<curve::BN254>* create_prover_crs(size_t 
 void commit(std::vector<barretenberg::fr>* input, size_t n, barretenberg::srs::factories::ProverCrs<curve::BN254>* crs)
 {
 
-    auto coeffs = br_fr_to_poly(input);
+    auto coeffs = std::make_shared<barretenberg::polynomial>(br_fr_to_poly(input));
+    auto crs_sp = std::make_shared<barretenberg::srs::factories::ProverCrs<curve::BN254>>(crs);
 
     transcript::StandardTranscript inp_tx = transcript::StandardTranscript(transcript::Manifest());
     plonk::KateCommitmentScheme<turbo_settings> newKate;
@@ -61,6 +62,5 @@ void commit(std::vector<barretenberg::fr>* input, size_t n, barretenberg::srs::f
 
     newKate.commit(coeffs->data(), "F_COMM", n, queue);
     queue.process_queue();
-    delete coeffs;
 }
 }
