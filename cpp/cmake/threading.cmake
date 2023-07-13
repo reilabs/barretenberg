1d2cab72ba407a29ae2729f3dcebef61481fcd64
Menko <themenkoprojects@gmail.com>
2023-07-13 15:54:30 +0200

    aaaa

--- cpp/cmake/threading.cmake
+++ cpp/cmake/threading.cmake
@@ -44,11 +44,9 @@ if (OPENMP_FOUND)
     set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
     set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
     # set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
+    target_link_libraries(${_name} OpenMP::OpenMP_CXX)
 endif(OPENMP_FOUND)
 
-if(TARGET OpenMP::OpenMP_CXX)
-    target_link_libraries(${_name} OpenMP::OpenMP_CXX)
-endif()
 
 if(DISABLE_TBB)
     message(STATUS "Intel Thread Building Blocks is disabled.")
